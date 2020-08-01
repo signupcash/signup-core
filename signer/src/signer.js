00c186c3ec73f88331ebba2a7cbd7ecf013dac68
p0o <pooria_az@ymail.com>
2020-08-01 17:43:10 +0800

    utility functions to handle worker

--- signer/src/signer.js
+++ signer/src/signer.js
@@ -19,8 +19,38 @@ import { notionLinkToBrowserCompatibility } from "./config";
 
 const bitboxWithSLP = new slpjs.BitboxNetwork(bitbox);
 
-let latestBalance;
+let myWorker;
+let workerEventListeners = [];
+
+export function initWorker() {
+  if (window.Worker) {
+    console.log("[SIGNUP][WORKER] Initiating the Worker...");
+
+    myWorker = new Worker("js/worker.lib.js");
+    myWorker.onmessage = function (e) {
+      const { data } = e;
+      if (workerEventListeners.some((x) => x.event === data.event)) {
+        workerEventListeners.forEach((listener) => {
+          if (listener.event === data.event) {
+            listener.cb.call(null, data);
+          }
+        });
+      }
+    };
+  } else {
+    // TODO connect to bridge without a worker
+    console.log("[SIGNUP] Your browser doesn't support web workers.");
+  }
+}
 
 export function handleMessageBackToClient(status, reqId, meta = {}) {
   window.opener.postMessage({ status, reqId, ...meta }, "*");
+}
+
+export function workerCourier(reqType, meta = {}) {
+  myWorker.postMessage({ reqType, ...meta });
+}
+
+export function onWorkerEvent(event, cb) {
+  workerEventListeners.push({ event, cb });
 }
