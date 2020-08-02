43adbc248fbb9d3e0b78ee0ee2ee94e1e019e09d
p0o <pooria_az@ymail.com>
2020-08-02 17:00:13 +0800

    show the txs in the popup

--- signer/src/components/wallet/RequestPermission.js
+++ signer/src/components/wallet/RequestPermission.js
@@ -1,7 +1,11 @@
 import { h, Fragment } from "preact";
-import { useState, useEffect } from "preact/hooks";
+import { useState, useEffect, useContext } from "preact/hooks";
 import { css } from "emotion";
-import { handleMessageBackToClient } from "../../signer";
+import {
+  handleMessageBackToClient,
+  workerCourier,
+  onWorkerEvent,
+} from "../../signer";
 import { makeSpendToken, makeSessionId } from "../../utils/permission";
 import { parseMoney } from "../../utils/unitUtils";
 import { getWalletEntropy, getBalance } from "../../utils/wallet";
@@ -9,6 +13,7 @@ import Heading from "../common/Heading";
 import Input from "../common/Input";
 import Button from "../common/Button";
 import Checkbox from "../common/Checkbox";
+import { UtxosContext } from "../WithUtxos";
 
 const permissionCss = css`
   margin: 16px;
@@ -18,19 +23,16 @@ const permissionCss = css`
 `;
 
 function connectWalletToTxBridge(sessionId) {
-  if (window.Worker) {
-    const myWorker = new Worker("js/worker.lib.js");
-    myWorker.postMessage({ reqType: "connect", sessionId });
-  } else {
-    // TODO connect to bridge without a worker
-    console.log("Your browser doesn't support web workers.");
-  }
+  workerCourier("connect", { sessionId });
 }
 
 export default function ({ clientPayload, bchAddr }) {
   const [status, setStatus] = useState("WAITING");
   const [balance, setBalance] = useState();
   const [balanceInUSD, setBalanceInUSD] = useState();
+  const [accomplishedTxs, setAccomplishedTxs] = useState([]);
+
+  const { refetchUtxos } = useContext(UtxosContext);
 
   useEffect(() => {
     (async () => {
@@ -41,6 +43,16 @@ export default function ({ clientPayload, bchAddr }) {
       setBalanceInUSD(balanceInUSD);
     })();
   }, [bchAddr]);
+
+  useEffect(() => {
+    refetchUtxos();
+    onWorkerEvent("tx", (eventData) => {
+      if (eventData.status === "DONE") {
+        setAccomplishedTxs([...accomplishedTxs, eventData]);
+        refetchUtxos();
+      }
+    });
+  }, []);
 
   async function handleAllow(e) {
     e.preventDefault();
@@ -71,77 +83,106 @@ export default function ({ clientPayload, bchAddr }) {
   }
 
   return (
-    <div class={permissionCss}>
-      {status === "WAITING" && (
-        <form onSubmit={handleAllow}>
-          {typeof balanceInUSD !== "undefined" &&
-            balanceInUSD < parseMoney(clientPayload.budget).value && (
-              <Heading highlight number={4} alert>
-                Your balance is only ${balanceInUSD}, it is not enough to meet
-                the required budget requested by the application. Click{" "}
-                <a href="/top-up">here</a> to top up your wallet with some BCH.
-              </Heading>
-            )}
-          <Heading number={5}>
-            Request for permission to spend from your wallet
-          </Heading>
-          <Heading number={4} inline>
-            From:
-          </Heading>
-          <Heading
-            number={4}
-            inline
-            customCss={css`
-              color: black;
-              margin: 8px 0;
-            `}
-          >
-            {clientPayload.origin}
-          </Heading>
-          <div
-            class={css`
-              display: flex;
-              flex-direction: row;
-            `}
-          >
-            <Heading number={4}>Budget:</Heading>
-            <Input
-              small
-              value={clientPayload.budget}
-              width="80px"
+    <>
+      <div class={permissionCss}>
+        {status === "WAITING" && (
+          <form onSubmit={handleAllow}>
+            {typeof balanceInUSD !== "undefined" &&
+              balanceInUSD < parseMoney(clientPayload.budget).value && (
+                <Heading highlight number={4} alert>
+                  Your balance is only ${balanceInUSD}, it is not enough to meet
+                  the required budget requested by the application. Click{" "}
+                  <a href="/top-up">here</a> to top up your wallet with some
+                  BCH.
+                </Heading>
+              )}
+            <Heading number={5}>
+              Request for permission to spend from your wallet
+            </Heading>
+            <Heading number={4} inline>
+              From:
+            </Heading>
+            <Heading
+              number={4}
+              inline
               customCss={css`
+                color: black;
                 margin: 8px 0;
               `}
-            />
-          </div>
+            >
+              {clientPayload.origin}
+            </Heading>
+            <div
+              class={css`
+                display: flex;
+                flex-direction: row;
+              `}
+            >
+              <Heading number={4}>Budget:</Heading>
+              <Input
+                small
+                value={clientPayload.budget}
+                width="80px"
+                customCss={css`
+                  margin: 8px 0;
+                `}
+              />
+            </div>
 
-          <Button type="submit" primary>
-            Allow
-          </Button>
-          <Button onClick={handleDeny} type="button" secondary>
-            Deny
-          </Button>
-        </form>
-      )}
+            <Button type="submit" primary>
+              Allow
+            </Button>
+            <Button onClick={handleDeny} type="button" secondary>
+              Deny
+            </Button>
+          </form>
+        )}
 
-      {status === "APPROVED" && (
-        <>
-          <Heading number={4}>Access Granted</Heading>
-          <Heading number={5} highlight>
-            You can now go back to the application. Please keep this window open
-            to allow {clientPayload.origin} to spend from your wallet up to{" "}
-            {clientPayload.budget}
-          </Heading>
-          <p
-            class={css`
-              margin: 32px 16px;
-            `}
-          >
-            Or simply revert back your permission by clicking the button below
+        {status === "APPROVED" && (
+          <>
+            <Heading number={4}>Access Granted</Heading>
+            <Heading number={5} highlight>
+              You can now go back to the application. Please keep this window
+              open to allow {clientPayload.origin} to spend from your wallet up
+              to {clientPayload.budget}
+            </Heading>
+            <p
+              class={css`
+                margin: 32px 16px;
+              `}
+            >
+              Or simply revert back your permission by clicking the button below
+            </p>
+            <Button>Revert Back Permission</Button>
+          </>
+        )}
+      </div>
+
+      {accomplishedTxs.map((tx) => (
+        <div
+          class={css`
+            background: #eee;
+            padding: 16px;
+            width: 100%;
+            padding: 16px;
+          `}
+        >
+          <Heading number={5}>Transaction: Done</Heading>
+          <p>
+            Spent {tx.action.amount} {tx.action.unit} to {tx.action.bchAddr}
           </p>
-          <Button>Revert Back Permission</Button>
-        </>
-      )}
-    </div>
+          <p>
+            Tx Id:{" "}
+            <a
+              href={`https://blockchair.com/bitcoin-cash/transaction/${tx.txResult.txId}`}
+              target="_blank"
+              rel="noopener noreferer"
+            >
+              {tx.txResult.txId.slice(0, 15)}...
+            </a>
+          </p>
+        </div>
+      ))}
+    </>
   );
 }
